764419c54650a46bddd15398fdf8ee49076caf94
SebekStash <sebekforgit@gmail.com>
2023-05-09 17:57:13 +0000

    [automation] Update pattern creator script

--- gitPatternCreator/createGithubPattern.sh
+++ gitPatternCreator/createGithubPattern.sh
@@ -4,10 +4,24 @@ LOGSFILE="./logs.log"
 DAY=$(date +%A)
 FULLDATE=$(date)
 
-if [ "$DAY" == "Tuesday" ];then
-    for i in {1..4};do
+if [ "$DAY" == "Tuesday" ] || [ "$DAY" == "Thursday" ];then
+    for i in {1..3};do
         echo $FULLDATE >> $LOGSFILE 
         git commit -a -m "Autocommit. Date: $FULLDATE"
     done
+    git push
+fi
+
+if [ "$DAY" == "Monday" ] || [ "$DAY" == "Friday" ];then
+    for i in {1..2};do
+        echo $FULLDATE >> $LOGSFILE 
+        git commit -a -m "Autocommit. Date: $FULLDATE"
+    done
+    git push
+fi
+
+if [ "$DAY" == "Sunday" ] || [ "$DAY" == "Saturday" ];then
+    echo $FULLDATE >> $LOGSFILE 
+    git commit -a -m "Autocommit. Date: $FULLDATE"
     git push
 fi
