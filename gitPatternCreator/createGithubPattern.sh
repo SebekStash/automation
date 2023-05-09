3a14766d74fd4926b05fb857d387734201fe4509
SebekStash <sebekforgit@gmail.com>
2023-05-09 17:50:16 +0000

    Autocommit. Date: Tue 09 May 2023 05:50:16 PM UTC

--- gitPatternCreator/createGithubPattern.sh
+++ gitPatternCreator/createGithubPattern.sh
@@ -3,12 +3,10 @@
 LOGSFILE="./logs.log"
 DAY=$(date +%A)
 FULLDATE=$(date)
-UPDATEGIT=$(git commit -a -m "Autocommit. Date: $FULLDATE";git push)
 
-# if [ "$DAY" == "Tuesday" ];then
-#     for i in {1..4};do
-#         echo $i
-#     done
-#     # echo $FULLDATE >> $LOGSFILE
-#     # echo $UPDATEGIT 
-# fi
+if [ "$DAY" == "Tuesday" ];then
+    for i in {1..4};do
+        echo $FULLDATE >> $LOGSFILE 
+        git commit -a -m "Autocommit. Date: $FULLDATE";git push
+    done
+fi
